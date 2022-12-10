75e85ea2378d89c3c02297e93c2c2860384bd207
Jaden D <45317852+GoldenJayz@users.noreply.github.com>
2022-12-10 07:44:38 -0500

    Add files via upload

--- classes/Database.ts
+++ classes/Database.ts
@@ -1,82 +1,63 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient } from "mongodb";
 
-export default class Database 
-{
-    private url: string;
-    private colName: string;
-    private collection: any;
-    private db: any;
-    private client: any;
+export default class Database {
+  private url: string;
+  private colName: string;
+  private collection: any;
+  private db: any;
+  private client: any;
 
-    public constructor(url: string, colName: string)
-    {
-        this.url = url;
-        this.colName = colName;
-        this.client = new MongoClient(this.url);
-        this.connect(this.colName);
+  public constructor(url: string, colName: string) {
+    this.url = url;
+    this.colName = colName;
+    this.client = new MongoClient(this.url);
+    this.connect(this.colName);
+  }
+
+  public connect(collectionName: string) {
+    try {
+      this.db = this.client.db("Spotify2YT");
+      this.collection = this.db.collection(collectionName);
+
+      return this.collection;
+    } catch (err) {
+      console.error(`Exception while connecting to collection: ${err}`);
+      throw err;
     }
+  }
 
-    public connect(collectionName: string)
-    {
-        try
-        {
-            this.db = this.client.db('Spotify2Yt');
-            this.collection = this.db.collection(collectionName);
-
-            return this.collection;
-        }
-        catch (err)
-        {
-            console.error(`Exception while connecting to collection: ${err}`);
-            throw err;
-        }
+  public listDocuments() {
+    try {
+      const documents = this.collection.find({}).toArray();
+      return documents;
+    } catch (err) {
+      throw err;
     }
+  }
 
-    public listDocuments()
-    {
-        try
-        {
-            const documents = this.collection.find({}).toArray();
-            return documents;
-        }
-        catch (err)
-        {
-            throw err;
-        }
+  public insertData(data: object[]) {
+    let res: any;
+
+    try {
+      if (data.length > 1) res = this.collection.insertMany(data);
+      else res = this.collection.insertOne(data[0]);
+      return res;
+    } catch (err) {
+      throw err;
     }
+  }
 
-    public insertData(data: object[])
-    {
-        let res: any;
-
-        try 
-        {
-            if (data.length > 1) res = this.collection.insertMany(data);
-            else res = this.collection.insertOne(data[0]);
-            return res;
-        }
-        catch (err)
-        {
-            throw err;
-        }
+  public removeAllData() {
+    let res: any;
+    try {
+      res = this.collection.deleteMany({});
+      return res;
+    } catch (err) {
+      throw err;
     }
+  }
 
-    public removeAllData()
-    {
-        let res: any;
-        try
-        {
-            res = this.collection.deleteMany({});
-            return res;
-        }
-        catch (err)
-        {
-            throw err;
-        }
-    }
-
-    public close()
-    {
-        this.client.close();
-    }
+  public close() {
+    this.client.close();
+  }
 }

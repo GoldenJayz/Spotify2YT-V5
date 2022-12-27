9d97930d110da30f389ef28f2954b2c330225b19
GoldenJayz <45317852+GoldenJayz@users.noreply.github.com>
2022-12-26 21:57:52 -0500

    started authentication

--- Helpers/Google.ts
+++ Helpers/Google.ts
@@ -9,16 +9,22 @@ const client = new google.auth.OAuth2(
   data.google.redirect_uri
 );
 
-const scopes = [
-    'https://www.googleapis.com/auth/youtube'
-];
+const yt = google.youtube({
+  version: "v3",
+  auth: client,
+});
 
-export const reqUrl = client.generateAuthUrl({ access_type: 'offline', scope: scopes, include_granted_scopes: true});
+const scopes = ["https://www.googleapis.com/auth/youtube"];
+
+export const reqUrl = client.generateAuthUrl({
+  access_type: "offline",
+  scope: scopes,
+  include_granted_scopes: true,
+});
 
 const curUser: string = queue[0]; // get cur user off queue when done
 
 console.log(reqUrl);
-
 
 // ------------------------------------------------------------
 //---------------------CALLBACK SECTION -----------------------
@@ -27,8 +33,8 @@ console.log(reqUrl);
 export const googleCallback = (req: any, res: any) => {
   const code = req.query.code!;
   client.getToken(code).then(getTokenRes);
-  return res.redirect('/');
-}
+  return res.redirect("/");
+};
 
 const getTokenRes = (res: any) => {
   let tokens: googleToken | null = res.tokens! != null ? res.tokens : null;
@@ -36,6 +42,17 @@ const getTokenRes = (res: any) => {
   // check if user is in the database and then if not create new doc in database if yes add to their user doc
   db.listDocuments(curUser).then((res: any) => console.log(res[0]));
 
+  yt.playlists.insert({
+    part: ["snippet"],
+    requestBody: {
+      snippet: {
+        title: "test",
+        description: "test",
+      },
+    },
+    })
+    .then((res: any) => console.log(res.data));
 
+  // get token then use it to create playlist and dump songs into it.
   // console.log(tokens);
-}
+};

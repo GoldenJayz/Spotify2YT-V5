aafade41335d45ce1c416678c9e0897382af111d
GoldenJayz <45317852+GoldenJayz@users.noreply.github.com>
2023-02-04 05:52:20 -0500

    Added back inserting songs into playlist

--- Helpers/Google.ts
+++ Helpers/Google.ts
@@ -74,15 +74,15 @@ const getTokenRes = (res: any) => {
 const playlistCreationRes = (res: any) => {
   playlistId = res.data.id; // Users playlist id that was just created
 
-  for (let i = 0; i < userSongs[curUser].length; i++) {
+  for (let i = 0; i < 5; i++) { // userSongs[curUser].length 5 is for testing it so it doesnt burn all my quota
     setTimeout(() => {
-      console.log(userSongs[curUser][i])
-      // yt.search.list({
-      //   part: ["snippet"],
-      //   maxResults: 1,
-      //   order: "relevance",
-      //   q: `${userSongs[curUser][i]}`,
-      // }).then(dumpIntoPlaylist);
+      // console.log(userSongs[curUser][i])
+      yt.search.list({
+        part: ["snippet"],
+        maxResults: 1,
+        order: "relevance",
+        q: `${userSongs[curUser][i]}`,
+      }).then(dumpIntoPlaylist);
     }, 1500*i);
   }
 };

f1623429135755f19349d396e36a1a2864ff4b91
GoldenJayz <45317852+GoldenJayz@users.noreply.github.com>
2023-07-02 10:25:18 -0400

    Stores spotifyAuth as cookie

---
+++ public/scripts/storeSpotifyAuth.js
@@ -0,0 +1,19 @@
+const urlParams = new URLSearchParams(window.location.search);
+const code = urlParams.get('code');
+
+// Switch this to public URL instead of testing URL 
+const url = 'http://localhost:6969/startAuth';
+fetch(url, {
+	method: 'POST',
+	headers: { 'Content-Type': 'application/json' },
+	body: JSON.stringify({
+		code: code
+	})
+})
+	.then(res => res.json())
+	.then(data => {
+		document.cookie = `spotifyAuth=${data.auth};`;
+
+		window.location.href = data.googleUrl;
+	})
+	.catch(err => console.error(err));

fe3970b0cd3f3c90017915fab6baabd2391ae801
GoldenJayz <45317852+GoldenJayz@users.noreply.github.com>
2023-07-03 08:18:24 -0400

    Added: Frontend storing google auth token

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import { Request, Response } from 'express';
 import { Logger } from 'tslog';
 import { PORT, URL } from './Helpers/exports';
 import { callbackFunc, postSpotify, startAuth } from './Helpers/Spotify';
-import { googleCallback } from './Helpers/Google';
+import { googleCallback, startGoogleAuth } from './Helpers/Google';
 import { createPayment, successPayment, validatePayment } from './Helpers/PayPal';
 import bodyParser from 'body-parser';
 import { exchangeTokens } from './Helpers/exchange';
@@ -19,7 +19,9 @@ app.get('/postSpotify', postSpotify);
 app.get('/callback', callbackFunc);
 app.get('/googleCallback', googleCallback);
 
+
 app.post('/startAuth', startAuth);
+app.post('/startGoogleAuth', startGoogleAuth);
 // app.get('/paypal', createPayment);
 // app.get('/paypal/success', (req: Request, res: Response) => { res.sendFile(__dirname + '/views/success.html'); });
 // app.post('/validatePayment', validatePayment);
